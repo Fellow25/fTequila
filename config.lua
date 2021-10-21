465ee3c70a7f3d5b3983cd73ad2dd53db1adf384
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-21 02:30:35 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,39 @@
+Tequila             = {}
+
+
+
+Tequila.jeveuxmarker = true --- true = Oui | false = Non
+
+Tequila.jeveuxblips = true --- true = Oui | false = Non
+
+Tequila.pos = {
+	coffre = {
+		position = {x = -574.08, y = 292.96, z = 79.18}
+	},
+	garage = {
+		position = {x = -563.9, y = 302.18, z = 82.98}
+	},
+	spawnvoiture = {
+		position = {x = -563.9, y = 302.18, z = 82.98, h = 263.91}
+	},
+	boss = {
+		position = {x = -572.74, y = 286.44, z = 79.18}
+	},
+	blips = {
+		position = {x = -565.15, y = 270.05, z = 83.02}
+	},
+	MenuPrendre = {
+		position = {x = -561.83, y = 287.27, z = 82.18}
+	},
+}
+
+Bar = {
+    item = {
+    {Label = "Pain", Name = "bread", Price = 1},
+    {Label = "Eau", Name = "water", Price = 1},
+    },
+}
+
+GTequilavoiture = {
+    {nom = "Blista", modele = "blista"},
+}
